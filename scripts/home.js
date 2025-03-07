a47d9b58d628f74de23b88d0e2bdd12964a01ef7
jose-a-m-ribeiro <macielribeirojoseaugusto@gmail.com>
2025-03-07 08:06:33 -0300

    Style do contato java script revisão

--- scripts/home.js
+++ scripts/home.js
@@ -1,11 +1,14 @@
-var btnExp = document.getElementsByClassName('.desce-menu')
-var menu = document.getElementsByClassName('.menu')
+var btnExp = document.getElementsByClassName('desce-menu')
+var menu = document.getElementsByClassName('menu')
 
 console.log(btnExp)
 console.log(menu)
 
-btnExp.addEventListener('click', expandeMenu())''
+for (let i = 0; i < btnExp.length; i++) {
+  btnExp.addEventListener('click', expandeMenu())
+}
 
 function expandeMenu() {
-  menu.classList.toggle('expandido')
+  for (let i = 0; i < menu.length; i++)
+    menu[i].classList.toggle('.expandido')
 }
